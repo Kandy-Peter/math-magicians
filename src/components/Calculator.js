f0587777d08c113151679622bf397e3e1a5c63fc
Kandy Peter <kandypeter03@gmail.com>
2022-02-14 20:41:01 +0200

    reduce line of codes

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,14 +4,13 @@ export default class Calculator extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+      numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0",".", "=", "AC", "%", "+/-"],
       operators: ["÷", "*", "-", "+"],
-      sign: [".", "=", "AC", "%", "+/-"],
       displayChars: "0",
     };
   }
   render() {
-    const btns = this.state;
+    const arrBtn = [...this.state.numbers, ...this.state.operators];
     return (
       <div className = "container-fluid">
         <div></div><div className="col-xs-12">
@@ -20,24 +19,11 @@ export default class Calculator extends React.Component {
             </div>
         </div>
         <div className = "row"></div>
-        <div className = "row"> <div></div>
+        <div className = "row">
           <div className = "buttonContainer">
-              {btns.numbers[6]}
-              {btns.numbers[3]}
-              {btns.numbers[0]}
-              {btns.sign[2]}
-              {btns.numbers[7]}
-              {btns.numbers[4]}
-              {btns.numbers[1]}
-              {btns.numbers[9]}
-              {btns.numbers[8]}
-              {btns.numbers[5]}
-              {btns.numbers[2]}
-              {btns.sign[0]}
-              {btns.operators}
-              {btns.sign[1]}
-              {btns.sign[4]}
-              {btns.sign[3]}  
+              {arrBtn.map((btn) => (<button type="button" key={btn}id={btn}>
+                {btn}
+              </button>))}
           </div>
         </div>
       </div>
