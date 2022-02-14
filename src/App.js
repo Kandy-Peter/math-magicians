cd8e45686405711d48eadfc8d36693120c19a2e6
Kandy Peter <kandypeter03@gmail.com>
2022-02-14 21:01:40 +0200

    add sass files and its dependecie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
 import Calculator from './components/Calculator.js';
 
 function App() {
   return (
     <div className="App">
-    Hello Devs
       <Calculator />
     </div>
   );
